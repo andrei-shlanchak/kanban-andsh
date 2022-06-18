96ffe2e3a9f8347dc1b6ba5cc245371e84ecfaab
Andy <Andy.Shlanchak@gmail.com>
2022-06-18 12:37:22 -0400

    port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const routes = require("./routes");
 const bodyParser = require("./bodyParser");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 bodyParser(app);
 routes(app);
