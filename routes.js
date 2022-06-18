d425ab409137d3628a413b3301f3268b804fd763
Andy <Andy.Shlanchak@gmail.com>
2022-06-18 12:19:09 -0400

    initial commit

---
+++ routes.js
@@ -0,0 +1,40 @@
+let cards = [
+    {id: '1', name: 'First card', status: 'todo', priority: 2},
+    {id: '2', name: 'Second card', status: 'progress', priority: 5},
+    {id: '3', name: 'Third card', status: 'review', priority: 10}
+];
+
+
+function routes(app) {
+
+
+    app.get('/', (req, res) => {
+        res.send('Hello World!')
+    })
+
+    app.get('/card', (req, res) => {
+        res.send(cards);
+    })
+    app.delete('/card/:cardId', (req, res) => {
+        const cardId = req.params.cardId;
+        cards = cards.filter(el => el.id !== cardId);
+        res.send(cards);
+    })
+
+
+    app.post('/card', (req, res) => {
+        const card = req.body;
+        cards.push({ id: Math.random().toString(), ...card});
+        res.send(cards)
+    })
+
+    app.patch('/card/:cardId', (req, res) => {
+        const cardId = req.params.cardId;
+        const card = req.body;
+        cards = cards.map(el => el.id === cardId ? ({...card, id: el.id}) : el)
+        res.send('Card updated')
+    })
+
+}
+
+module.exports = routes;
